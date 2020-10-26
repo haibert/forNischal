b71cbd7a7f8d97f6a1e885390c87f9ce9e29a5f1
haibert <haibert@simplyrem.com>
2020-10-26 16:28:00 -0700

    Initial Commit

---
+++ for forNischal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  for forNischal
+//
+//  Created by hb on 10/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
