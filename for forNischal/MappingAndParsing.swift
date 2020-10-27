d6179137038b562f9c13a7fcc344088fd4d87631
Nischal Hada <hadanischal@gmail.com>
2020-10-27 11:08:02 +1100

    [Update MappingAndParsing] Updated MappingAndParsing

--- for forNischal/MappingAndParsing.swift
+++ for forNischal/MappingAndParsing.swift
@@ -33,17 +33,33 @@ struct DataClass: Codable {
     
     // THIS IS SO I CAN DECODE THE DATA AND STORE IT LOCALLY....
     init(from decoder: Decoder) throws {
-        if let dataContainer = try? decoder.container(keyedBy: CodingKeys.self) {
-            self.userID = try dataContainer.decode(String.self, forKey: .userID)
-            self.clientID = try dataContainer.decode(String.self, forKey: .clientID)
-            self.Name = try dataContainer.decode(String.self, forKey: .Name)
-            self.lastName = try dataContainer.decode(String.self, forKey: .lastName)
-            self.emailAddress = try dataContainer.decode(String.self, forKey: .emailAddress)
-            self.techID = try dataContainer.decode(String.self, forKey: .techID)
-            self.additionalCompanies = try dataContainer.decode(String.self, forKey: .additionalCompanies)
-            
-        }
+        let dataContainer = try decoder.container(keyedBy: CodingKeys.self)
+        self.userID = try dataContainer.decode(String.self, forKey: .userID)
+        self.clientID = try dataContainer.decode(String.self, forKey: .clientID)
+        self.Name = try dataContainer.decode(String.self, forKey: .Name)
+        self.lastName = try dataContainer.decode(String.self, forKey: .lastName)
+        self.emailAddress = try dataContainer.decode(String.self, forKey: .emailAddress)
+        self.techID = try dataContainer.decode(String.self, forKey: .techID)
+        self.additionalCompanies = try dataContainer.decode(String.self, forKey: .additionalCompanies)
+        self.clientDetails = try dataContainer.decode(ClientDetails.self, forKey: .clientDetails)
+
     }
+    // Instead of
+   // if let dataContainer = try? decoder.container(keyedBy: CodingKeys.self)  {}
+
+    /*
+    init(from decoder: Decoder) throws {
+        let dataContainer = try decoder.container(keyedBy: CodingKeys.self)
+        self.userID = try dataContainer.decodeIfPresent(String.self, forKey: .userID)
+        self.clientID = try dataContainer.decodeIfPresent(String.self, forKey: .clientID)
+        self.Name = try dataContainer.decodeIfPresent(String.self, forKey: .Name)
+        self.lastName = try dataContainer.decodeIfPresent(String.self, forKey: .lastName)
+        self.emailAddress = try dataContainer.decodeIfPresent(String.self, forKey: .emailAddress)
+        self.techID = try dataContainer.decodeIfPresent(String.self, forKey: .techID)
+        self.additionalCompanies = try dataContainer.decodeIfPresent(String.self, forKey: .additionalCompanies)
+        self.clientDetails = try dataContainer.decodeIfPresent(ClientDetails.self, forKey: .clientDetails)
+    }
+ */
 }
 // MARK: - ClientDetails
 struct ClientDetails: Codable {
@@ -90,26 +106,24 @@ struct ClientDetails: Codable {
 
     
     init(from decoder: Decoder) throws {
-        if let dataContainer = try? decoder.container(keyedBy: CodingKeys.self) {
-            self.clientID = try dataContainer.decode(String.self, forKey: .clientID)
-            self.firstName = try dataContainer.decode(String.self, forKey: .firstName)
-            self.lastName = try dataContainer.decode(String.self, forKey: .lastName)
-            self.email = try dataContainer.decode(String.self, forKey: .email)
-            self.customerName = try dataContainer.decode(String.self, forKey: .customerName)
-            self.businessAddress = try dataContainer.decode(String.self, forKey: .businessAddress)
-            self.businessCity = try dataContainer.decode(String.self, forKey: .businessCity)
-            self.businessState = try dataContainer.decode(String.self, forKey: .businessState)
-            self.businessZip = try dataContainer.decode(String.self, forKey: .businessZip)
-            self.businessCountry = try dataContainer.decode(String.self, forKey: .businessCountry)
-            self.businessPhone = try dataContainer.decode(String.self, forKey: .businessPhone)
-            self.businessFax = try dataContainer.decode(String.self, forKey: .businessFax)
-            self.mobilePhone = try dataContainer.decode(String.self, forKey: .mobilePhone)
-            self.contactName = try dataContainer.decode(String.self, forKey: .contactName)
-            self.contactTitle = try dataContainer.decode(String.self, forKey: .contactTitle)
-            self.contactPhone = try dataContainer.decode(String.self, forKey: .contactPhone)
-            self.contactEmail = try dataContainer.decode(String.self, forKey: .contactEmail)
-            self.lastUpdated = try dataContainer.decode(String.self, forKey: .lastUpdated)
-            
-        }
+        let dataContainer = try decoder.container(keyedBy: CodingKeys.self)
+        self.clientID = try dataContainer.decode(String.self, forKey: .clientID)
+        self.firstName = try dataContainer.decode(String.self, forKey: .firstName)
+        self.lastName = try dataContainer.decode(String.self, forKey: .lastName)
+        self.email = try dataContainer.decode(String.self, forKey: .email)
+        self.customerName = try dataContainer.decode(String.self, forKey: .customerName)
+        self.businessAddress = try dataContainer.decode(String.self, forKey: .businessAddress)
+        self.businessCity = try dataContainer.decode(String.self, forKey: .businessCity)
+        self.businessState = try dataContainer.decode(String.self, forKey: .businessState)
+        self.businessZip = try dataContainer.decode(String.self, forKey: .businessZip)
+        self.businessCountry = try dataContainer.decode(String.self, forKey: .businessCountry)
+        self.businessPhone = try dataContainer.decode(String.self, forKey: .businessPhone)
+        self.businessFax = try dataContainer.decode(String.self, forKey: .businessFax)
+        self.mobilePhone = try dataContainer.decode(String.self, forKey: .mobilePhone)
+        self.contactName = try dataContainer.decode(String.self, forKey: .contactName)
+        self.contactTitle = try dataContainer.decode(String.self, forKey: .contactTitle)
+        self.contactPhone = try dataContainer.decode(String.self, forKey: .contactPhone)
+        self.contactEmail = try dataContainer.decode(String.self, forKey: .contactEmail)
+        self.lastUpdated = try dataContainer.decode(String.self, forKey: .lastUpdated)
     }
 }
